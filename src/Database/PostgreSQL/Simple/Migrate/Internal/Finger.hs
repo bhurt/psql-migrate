2cce712176610eb7a547573c6f4c0fe7e6fff274
Brian Hurt <bhurt42@gmail.com>
2025-04-14 09:38:34 -0500

    Get off the now deprecated cryptonite.
    This requires us to switch our hashing algorithm from SHA3-256 back to
    SHA2-256.  Which I'm not happy with, but I'm not willing to fight.

--- src/Database/PostgreSQL/Simple/Migrate/Internal/Finger.hs
+++ src/Database/PostgreSQL/Simple/Migrate/Internal/Finger.hs
@@ -18,10 +18,11 @@ module Database.PostgreSQL.Simple.Migrate.Internal.Finger (
     makeFingerprint
 ) where
 
-    import qualified Crypto.Hash                      as Hash
-    import qualified Data.ByteArray                   as ByteArray
+    import qualified Data.Binary                      as Binary
     import           Data.ByteString                  (ByteString)
     import qualified Data.ByteString.Base64.URL       as Base64
+    import qualified Data.ByteString.Lazy             as Lazy
+    import qualified Data.Digest.Pure.SHA             as Hash
     import           Data.Text                        (Text)
     import qualified Data.Text.Encoding               as Encoding
     import qualified Database.PostgreSQL.Simple.Types as PQ
@@ -29,14 +30,20 @@ module Database.PostgreSQL.Simple.Migrate.Internal.Finger (
     -- | Calculate the fingerprint of a given migration command.
     makeFingerprint :: PQ.Query -> Text
     makeFingerprint cmd =
-        let q :: ByteString
-            q = PQ.fromQuery cmd
+        let q0 :: ByteString
+            q0 = PQ.fromQuery cmd
 
-            dig :: Hash.Digest Hash.SHA3_256
-            dig = Hash.hash q
+            q :: Lazy.ByteString
+            q = Lazy.fromStrict q0
+
+            dig :: Hash.Digest Hash.SHA256State
+            dig = Hash.sha256 q
+
+            bs0 :: Lazy.ByteString
+            bs0 = Binary.encode dig
 
             bs :: ByteString
-            bs = ByteArray.convert dig
+            bs = Lazy.toStrict bs0
 
             b64 :: ByteString
             b64 = Base64.encode bs
